b58217be009411ef0d8de9f1cd486b2166385285
Christer van der Meeren <christer@infotech.no>
2022-01-13 10:33:12 +0100

    Remove unused code

--- src/Fling.Tests/Utils.fs
+++ src/Fling.Tests/Utils.fs
@@ -8,14 +8,3 @@ module List =
 
   let isDistinctBy f xs =
     xs |> List.distinctBy f |> List.length = xs.Length
-
-
-
-module Map =
-
-
-  let keys map =
-    map
-    |> Map.toSeq
-    |> Seq.map fst
-    |> set
